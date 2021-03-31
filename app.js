09944f262e76475d9128fe5eeede0543cc58a73e
acmahaja <acmahaja@gmail.com>
2021-03-31 05:18:45 +0200

    Fixed styling error for desktop mode by adding script folder

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ app.set('view engine', 'ejs')
 
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/script'));
 
 
 app.get('/', (req, res) => {
     res.render('index');
 })
 
+app.all('*', (req, res) => {
+    res.redirect('/');
+})
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Listening on port 3000");
 })
