af74e2f2ecfc8f64b7f17db3ddb246ae35f09cb3
acmahaja <acmahaja@gmail.com>
2021-07-26 14:31:08 +0200

    Backup commit

--- app.js
+++ app.js
@@ -1,228 +1,24 @@
-< !DOCTYPE html >
-    <
-    html lang = "en" >
+const express = require('express');
+const app = express();
 
-    <
-    head >
-    <
-    meta charset = "UTF-8" >
-    <
-    meta http - equiv = "X-UA-Compatible"
-content = "IE=edge" >
-    <
-    meta name = "viewport"
-content = "width=device-width, initial-scale=1.0" >
-    <
-    title > acmahaja < /title> <
-    link rel = "stylesheet"
-href = "/css/index.css" >
+//const puppeteer = require('puppeteer');
 
 
-    <
-    link rel = "stylesheet"
-href = "https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-integrity = "sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
-crossorigin = "anonymous" >
+
+var path = require('path');
 
 
-    <
-    meta property = "og:title"
-content = "Portfolio site for @acmahaja" / >
-    <
-    meta property = "og:description"
-content = "I am a graduate software engineering student who has a passion for product design and its innovation. I work well with people who are passionate about advancing consumer products and pushing for increased access to essential technologies for lower and
-middle - income communities " /> <
-    meta property = "og:url"
-content = "https://www.acmahaja.com/" / >
-    <
-    meta property = "og:image"
-content = "./images/profile.png" / >
+app.set('view engine', 'ejs');
 
-    <
-    /head>
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(__dirname + '/public'));
 
-<
-body >
-    <
-    nav >
-    <
-    a class = "link" > Home < /a> <
-    div class = "links" >
-    <
-    div class = "link" > < a class = "link" > Github < /a></div >
-    <
-    div class = "link" > < a class = "link" > Projects < /a></div >
-    <
-    div class = "link" > < a class = "link" > CV🔗 < /a></div >
 
-    <
-    /div> <
-    /nav> <
-    div class = "ver" >
-    <
-    div class = "hor" >
-    <
-    div class = "bio" >
-    <
-    div class = "profile" >
-    <
-    img src = "/images/profile.png"
-alt = "profile pic" >
-    <
-    h1 class = "name" > Anjaney C Mahajan < /h1> <
-    h3 class = "username" > @acmahaja < /h3> <
-    /div> <
-    div class = "descr" >
-    I am a graduate computer engineering student who has a passion
-for product design and its innovation.I work well with people who are passionate about advancing consumer products and pushing
-for increased access to essential technologies
-for lower and
-middle - income communities <
-    /div> <
-    /div> <
-    div class = "websites" >
-    <
-    div class = "website email grid-item" >
-    <
-    div class = "banner" >
-    <
-    em class = "fas fa-envelope" > < /em> <
-    div class = "center" >
-    <
-    h4 > acmahaja @gmail.com < /h4> <
-    /div> <
-    /div> <
-    div class = "web-content" >
-    <
-    form action = "mailto:acmahaja@gmail.com"
-method = "post" >
-    <
-    div class = "email-form" >
-    <
-    label
-for = "name" > Name: < /label> <
-    input type = "text"
-name = "name"
-class = "name" >
-    <
-    /div> <
-    div class = "email-form" >
-    <
-    label
-for = "name" > Email: < /label> <
-    input type = "text"
-name = "mail"
-class = "mail" >
+app.get('/', async(req, res) => {
+    res.render('pages/index');
+});
 
-    <
-    /div> <
-    div class = "email-form" >
-    <
-    label
-for = "name" > Comment: < /label> <
-    input type = "text"
-name = "comment"
-id = "comment" >
-    <
-    /div> <
-    div class = "email-button" >
-    <
-    input type = "submit"
-value = "Send" >
-    <
-    input type = "reset"
-value = "Reset" >
-    <
-    /div> <
-    /form> <
-    /div> <
-    /div> <
-    div class = "website linkedin grid-item" >
-    <
-    div class = "banner" >
-    <
-    em class = "fab fa-linkedin  " > < /em> <
-    div class = "center" >
-    <
-    h4 > /ln/linkedin < /h4> <
-    /div> <
-    /div> <
-    div class = "web-content" >
-
-    <
-    /div> <
-    /div> <
-    div class = "website github grid-item" >
-    <
-    div class = "banner" >
-    <
-    em class = "fab fa-github  " > < /em> <
-    div class = "center" >
-    <
-    h4 > /acmahaja</h
-4 >
-    <
-    /div> <
-    /div> <
-    div class = "web-content" >
-    <
-    /div> <
-    /div> <
-    div class = "website discord grid-item" >
-    <
-    div class = "banner" >
-    <
-    em class = "fab fa-discord  " > < /em> <
-    div class = "center" >
-    <
-    h4 > acmahaja #0052</h4>
-
-                        </div>
-
-                    </div>
-
-                    <div class= "web-content" >
-
-    <
-    /div> <
-    /div> <
-    div class = "website instagram grid-item" >
-    <
-    div class = "banner" >
-    <
-    em class = "fab fa-instagram  " > < /em> <
-    div class = "center" >
-    <
-    h4 > @acmahaja < /h4> <
-    /div> <
-    /div> <
-    div class = "web-content" >
-    <
-    /div> <
-    /div> <
-    div class = "website medium grid-item" >
-    <
-    div class = "banner" >
-    <
-    em class = "fab fa-medium  " > < /em> <
-    div class = "center" >
-    <
-    h4 > /@acmahaja</h
-4 >
-    <
-    /div> <
-    /div> <
-    div class = "web-content" >
-    <
-    /div> <
-    /div> <
-    /div> <
-    /div> <
-    /div>
-
-<
-script src = "/js/index.js" > < /script> <
-    /body>
-
-<
-/html>
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
