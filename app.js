bf05b0fe395d16a71a3b4f0f9970ea640b75c2eb
anjaney <acmahaja@gmail.com>
2021-11-13 11:17:53 +0200

    backup commit

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 
+const axios = require('axios')
+
+app.set('trust proxy', true)
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.login(process.env.TOKEN)
 
 const path = require('path')
 
@@ -8,6 +18,17 @@ app.set('view engine', 'ejs')
 
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + '/public'));
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", msg => {
+    if (msg.content === "ping") {
+        msg.reply("pog");
+    }
+})
+
 
 app.get('/', (req, res) => {
     res.render('pages/index');
