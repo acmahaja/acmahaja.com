4bdfd95c9783f4412072a7d0b9ce0f614f88b94b
@acmahaja <acmahaja@gmail.com>
2022-06-20 11:29:27 +0200

    Added starter problems

--- src/App.js
+++ src/App.js
@@ -1,31 +1,65 @@
-import React from "react";
-import {AiFillGithub} from "react-icons/ai"
-import "./css/App.css";
-import "./css/building-blocks.css";
+import React from 'react';
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>{props.name != null ? 'Hello ' + props.name + ',' : null} {props.age != null ? 'you are ' + props.age + ' years old' : null}</p>
+    </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div className="">
+      greeting app created by <a href="https://github.com/acmahaja">acmahaja</a>
+    </div>
+  )
+}
 
 
 const App = () => {
+  // const now = new Date();
+  // const a = 10;
+  // const b = 20;
 
-  const goToRepo = () => {
-    console.log("asd");
-    window.location.href = "https://github.com/acmahaja/acmahaja.com"
-    // return null
-  }
+  // return (
+  //   <div>
+  //     <p>Hello world, it is {now.toString()}</p>
+  //     <p>
+  //       {a} plus {b} is {a + b}
+  //     </p>
+  //   </div>
+  // );
+
+  // return React.createElement(
+  //   'div',
+  //   null,
+  //   React.createElement('p', null, 'Hello World, it is', now.toString()),
+  //   React.createElement('p', null, a, ' plus ', b, ' is ', a + b),
+  // )
+
+  // return (
+  //   <div className="">
+  //     <h1>Greetings</h1>
+  //     <Hello />
+  //     <Hello name="George" />
+  //     <Hello name="Daisy" />
+  //   </div>
+  // )
+
+  const name = 'Peter'
+  const age = 10
 
   return (
     <div className="App">
-      <div className="cloudBlur" />
-      <h2>Hi, my name is ...</h2>
-      <h1>Anjaney C Mahajan</h1>
-      <p>
-        I am overhauling my personal website, track my progress here on
-        <span onClick={goToRepo}>
-            Github 
-            <AiFillGithub size={35}/>
-        </span>
-      </p>
+      <h1>Greetings</h1>
+      <Hello />
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+      <Footer />
     </div>
-  );
-};
+  )
+
+}
 
 export default App;
