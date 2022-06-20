17d0dacfa704772f862e24f23395bfb19f5ed8b1
@acmahaja <acmahaja@gmail.com>
2022-06-20 15:13:35 +0200

    Added starter files

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
+import {AiFillGithub} from "react-icons/ai"
+import "./css/App.css";
+import "./css/building-blocks.css";
+
 
 const App = () => {
+
+  const goToRepo = () => {
+    console.log("asd");
+    window.location.href = "https://github.com/acmahaja/acmahaja.com"
+    // return null
+  }
+
   return (
     <div className="App">
+      <div className="cloudBlur" />
+      <h2>Hi, my name is ...</h2>
+      <h1>Anjaney C Mahajan</h1>
+      <p>
+        I am overhauling my personal website, track my progress here on
+        <span onClick={goToRepo}>
+            Github 
+            <AiFillGithub size={35}/>
+        </span>
+      </p>
     </div>
-  )
-
-}
+  );
+};
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+const dotenv = require('dotenv')
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+
+app.get('*', (req, res) => {
+    res.send({message:'Welcome to the backend, this place is currently under construction 🔨'})
 })
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.port || 8080;
 app.listen(PORT, ()=> {
     console.log(`Server is running on ` + PORT)
 })
