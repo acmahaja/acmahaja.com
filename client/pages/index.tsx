ca0d2410f82c8692508021162094802a99ccf37a
@acmahaja <acmahaja@gmail.com>
2022-11-07 16:08:10 +0200

    removed .com

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://github.com/acmahaja">acmahaja.com</a>
+          Welcome to <a href="https://github.com/acmahaja">acmahaja</a>
         </h1>
       </main>
     </div>
