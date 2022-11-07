209f25a9f36e28f918038771b529ebde9a6a8421
@acmahaja <acmahaja@gmail.com>
2022-11-07 20:17:36 +0200

    Removed Styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,17 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://github.com/acmahaja">acmahaja.com</a>
-        </h1>
-      </main>
+    <div className="Home">
+      asd
     </div>
   )
 }
+
+
+    // <div className={styles.container}>
+    //   <main className={styles.main}>
+    //     <h1 className={styles.title}>
+    //       Welcome to <a href="https://github.com/acmahaja">acmahaja.com</a>
+    //     </h1>
+    //   </main>
+    // </div>
