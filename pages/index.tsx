1255fe903e97b4995e2daa98f2eb22dc6608384e
@acmahaja <acmahaja@gmail.com>
2022-11-07 18:14:31 +0200

    modified tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://github.com/acmahaja">acmahaja</a>
+          Welcome to <a href="https://github.com/acmahaja">acmahaja.com</a>
         </h1>
       </main>
     </div>
