5479a54fa124e9c1d4c515d4218ff69ca8310f72
acmahaja <acmahaja@gmail.com>
2023-01-08 14:17:47 +0200

    Added mobile viewport styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import ConstructionWorker from './assets/Construction Worker.svg';
-import './App.css';
+import ConstructionWorker from "./assets/Construction Worker.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <section>
-      <img src={ConstructionWorker} className="App-logo" alt="Construction Worker" />        
+        {/* <img
+          src={ConstructionWorker}
+          className="App-logo"
+          alt="Construction Worker"
+        />
+        <div className="text">
+          
+        </div> */}
       </section>
     </div>
   );
