9a0e2f059e4eed584dca831c8fb8a0d4a5b7c470
acmahaja <acmahaja@gmail.com>
2023-01-08 10:06:09 +0200

    restarting to new design

--- pages/components/Logo.tsx
+++
@@ -1,9 +0,0 @@
-import styles from '../../styles/Components/Logo.module.css'
-export default function Logo(){
-    return (<div className={styles.Logo}>
-        <b>/</b>
-        <div>
-        acmahaja
-        </div>
-    </div>)
-}
