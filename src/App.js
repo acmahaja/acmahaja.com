01583ba168c0f3c853e648dd6e9966f65f4c3376
acmahaja <acmahaja@gmail.com>
2023-01-08 13:42:36 +0200

    Added main construction page section element and image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import ConstructionWorker from './assets/Construction Worker.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+      <img src={ConstructionWorker} className="App-logo" alt="Construction Worker" />        
+      </section>
     </div>
   );
 }
