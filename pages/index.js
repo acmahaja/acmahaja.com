597d2cbe9f059d4d1a7f5ed71bdc0b616fb0d864
acmahaja <acmahaja@gmail.com>
2023-05-12 13:40:31 +1000

    Added Header

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,69 @@
 import Head from "next/head";
 
 export default function Home() {
-
   return (
     <>
-      <Head>
-        <title>acmahaja.com</title>
-        <meta
-          name="description"
-          content="Hi, my name is Anjaney C Mahajan, I am overhauling my personal website, track my progress here on Github."
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+        <Head>
+          <meta name="description" content="" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          // Primary Meta Tags
+          <title>@acmahaja</title>
+          <meta name="title" content="@acmahaja"></meta>
+          <meta
+            name="description"
+            content="I am a software engineering with business student at the University of Melbourne, I completed my undergraduate degree in Computer Engineering at North Carolina State University. "
+          ></meta>
+          <link rel="icon" href="/favicon.ico" />
+          <meta
+            property="og:image"
+            content="/android-chrome-192x192.png"
+          ></meta>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          // Open Graph / Facebook
+          <meta property="og:type" content="website"></meta>
+          <meta property="og:url" content="https://www.acmahaja.com/"></meta>
+          <meta property="og:title" content="@acmahaja"></meta>
+          <meta
+            property="og:description"
+            content="I am a software engineering with business student at the University of Melbourne, I completed my undergraduate degree in Computer Engineering at North Carolina State University."
+          ></meta>
+          <meta
+            property="og:image"
+            content="https://www.acmahaja.com/android-chrome-512x512.png"
+          ></meta>
+          // Twitter
+          <meta property="twitter:card" content="summary_large_image"></meta>
+          <meta
+            property="twitter:url"
+            content="https://www.acmahaja.com/"
+          ></meta>
+          <meta property="twitter:title" content="@acmahaja"></meta>
+          <meta
+            property="twitter:description"
+            content="I am a software engineering with business student at the University of Melbourne, I completed my undergraduate degree in Computer Engineering at North Carolina State University."
+          ></meta>
+          <meta
+            property="twitter:image"
+            content="https://www.acmahaja.com/android-chrome-512x512.png"
+          ></meta>
+        </Head>
 
       <main className={styles.Main}>
 
