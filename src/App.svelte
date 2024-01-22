2b61a4710a8e43f732f10ecde1e4e29054309855
@acmahaja <acmahaja@gmail.com>
2024-01-22 20:44:54 +1100

    Added Roboto fonts

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
 
 <main>
   <div class="flex items-center justify-center h-screen">
-    <h1 class="text-4xl font-bold text-blue-500">
+    <h1 class="font-roboto text-4xl font-bold text-green-300">
       acmahaja.com
     </h1>
   </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
+    fontFamily: {
+      'sans-serif': ['Roboto Flex', 'sans-serif'],
+      'serif': ['Roboto Serif', 'serif'],
+      'mono': ['Roboto Mono', 'monospace'],
+
+    }
   },
   plugins: [],
 }
