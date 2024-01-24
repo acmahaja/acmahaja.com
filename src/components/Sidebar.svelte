c2e610b0a4f618278c4e7803bdc804ce94fea573
@acmahaja <acmahaja@gmail.com>
2024-01-24 16:38:13 +1100

    Fixed click sidebar button's width

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import anime  from "animejs/lib/anime";
 
-    let toggleViewSideBar:boolean = true;
+    let toggleViewSideBar:boolean = false;
 
     function toggleSideBar() {
         anime({
@@ -13,7 +13,7 @@
     }
 </script>
 
-<nav class="flex flex-row justify-between align-center sm:hidden p-5 bg-black">
-    <p class="font-mono font-semibold text-white hover:underline hover:cursor-pointer hover:underline-offset-4">@acmahaja</p>
-    <img id="toggleSideBarButton" src="/double_arrow.svg" on:click={toggleSideBar} alt="sidebar-toggle" />
+<nav class="flex flex-row justify-between items-center sm:hidden p-5 bg-black">
+    <p class="font-mono font-semibold text-white leading-[0px] hover:underline hover:cursor-pointer hover:underline-offset-4">@acmahaja</p>
+    <button id="toggleSideBarButton" class="bg-[url('/double_arrow.svg')] w-4 h-4 bg-no-repeat bg-center" on:click={toggleSideBar}></button>
 </nav>
