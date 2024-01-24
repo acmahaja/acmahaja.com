624b673ca0f8509706001f54c3f1d0038de92ffc
@acmahaja <acmahaja@gmail.com>
2024-01-24 16:18:18 +1100

    Added animation when sidebar button is clicked

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -1,4 +1,19 @@
-<script lang="ts"></script>
+<script lang="ts">
+    import anime  from "animejs/lib/anime.es";
 
-<!-- TODO: FINISH -->
-<nav class="flex sm:hidden">Sidebar</nav>
+    let toggleViewSideBar:boolean = true;
+
+    function toggleSideBar() {
+        anime({
+            targets: '#toggleSideBarButton',
+            rotate: toggleViewSideBar ? "0.5turn" : "0",
+            autoplay: true,
+        })
+        toggleViewSideBar = !toggleViewSideBar;
+    }
+</script>
+
+<nav class="flex flex-row justify-between align-center sm:hidden p-5 bg-black">
+    <p class="font-mono font-semibold text-white hover:underline hover:cursor-pointer hover:underline-offset-4">@acmahaja</p>
+    <img id="toggleSideBarButton" src="/double_arrow.svg" on:click={toggleSideBar} alt="sidebar-toggle" />
+</nav>
