a0475dc9c4d6335a78945c6a16def1ea450a760d
@acmahaja <acmahaja@gmail.com>
2024-01-24 17:45:26 +1100

    Completed Navbar with pushDown Animation

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,27 @@
 <script lang="ts">
-  import NavBar from "./components/NavBar.svelte";
+  import anime from "animejs";
+import NavBar from "./components/NavBar.svelte";
   import Sidebar from "./components/Sidebar.svelte";
+
+  let pushDown:boolean = false;
+
+  function togglePushDown() {
+    pushDown = !pushDown
+
+    console.log("toggle push down " + pushDown);
+
+    anime({
+      targets : '#main',
+      paddingTop: pushDown ? "8.75rem" : "0rem",
+      autoplay: true,
+    })
+  }
 </script>
 
 <NavBar />
-<Sidebar />
-<main>
-  <div class="flex items-center justify-center h-screen">
+<Sidebar on:change={togglePushDown} />
+<main id="main" class="flex items-center justify-center h-screen bg-green">
     <h1 class="font-roboto text-4xl font-bold text-green-300">
       acmahaja.com
     </h1>
-  </div>
 </main>
--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
     import anime  from "animejs/lib/anime";
+    import { createEventDispatcher } from 'svelte';
 
-    let toggleViewSideBar:boolean = false;
+    const dispatch = createEventDispatcher()
+
+
+    let toggleViewSideBar:boolean = true;
 
     function toggleSideBar() {
         anime({
@@ -9,11 +13,27 @@
             rotate: toggleViewSideBar ? "0.5turn" : "0",
             autoplay: true,
         })
+
+        anime({
+            targets: '#sidebar',
+            autoplay: true,
+            height: !toggleViewSideBar ? "0" : "8.75rem",
+        })
+
         toggleViewSideBar = !toggleViewSideBar;
+        dispatch('change');
     }
 </script>
 
-<nav class="flex flex-row justify-between items-center sm:hidden p-5 bg-black">
+<nav class="flex flex-row justify-between items-center fixed w-screen h-16 px-5 bg-black sm:hidden">
     <p class="font-mono font-semibold text-white leading-[0px] hover:underline hover:cursor-pointer hover:underline-offset-4">@acmahaja</p>
     <button id="toggleSideBarButton" class="bg-[url('/double_arrow.svg')] w-4 h-4 bg-no-repeat bg-center" on:click={toggleSideBar}></button>
 </nav>
+
+<div id="sidebar" class="bg-black h-0 w-dvw fixed top-16 overflow-hidden">
+    <ul class="w-full max-w-[70rem] flex flex-col align-middle text-right p-4 gap-4 ">
+        <li class="font-mono font-medium text-primary hover:underline hover:cursor-pointer hover:underline-offset-4">About</li>
+        <li class="font-mono font-medium text-primary hover:underline hover:cursor-pointer hover:underline-offset-4">Background</li>
+        <li class="font-mono font-medium text-primary hover:underline hover:cursor-pointer hover:underline-offset-4">Projects</li>
+    </ul>
+</div>
