7ba845b165be97c99ac3c50be862e2f01ea9e7e2
@acmahaja <acmahaja@gmail.com>
2024-01-28 13:54:48 +1100

    Added more test images

--- src/data/Images.ts
+++ src/data/Images.ts
@@ -1,5 +1,7 @@
 const images:string[] = [
   'https://www.w3schools.com/html/pic_trulli.jpg',
+  'https://res.cloudinary.com/www-acmahaja-com/image/upload/v1657613135/cld-sample.jpg',
+  'https://res.cloudinary.com/www-acmahaja-com/image/upload/v1657613136/cld-sample-2.jpg'
 ]
 
 export default images;
