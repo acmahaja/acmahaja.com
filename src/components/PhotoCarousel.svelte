a2ace11d1b02e16dbec1faa2e7ff2df60dc6dfa1
@acmahaja <acmahaja@gmail.com>
2024-01-28 12:30:15 +1100

    Added image carosel

--- src/components/PhotoCarousel.svelte
+++ src/components/PhotoCarousel.svelte
@@ -1,9 +1,27 @@
-<script lang="ts"></script>
+<script lang="ts">
+    import Carousel from 'svelte-carousel';
+    import images from '../data/Images';
+</script>
 
-<div id="AboutDescription" class="w-full min-h-fit bg-white font-flex font-medium border-solid border-black border-[1px] rounded-md flex flex-col ">
-    <div id="AboutTitlebar" class=" h-fit border-solid size-8 p-2 w-full border-black border-b-[1px] px-3 bg-[#D3FF5C] flex items-center">
-        <p class="font-mono bg-black text-white p-1 px-4 rounded-xl">About</p>
+<div id="PhotoCarousel" class="w-full min-h-fit bg-background font-flex font-medium border-solid border-black border-[1px] rounded-md flex flex-col ">
+    <div id="PhotoCarousel" class=" h-fit border-solid size-8 p-2 w-full border-black border-b-[1px] px-3 bg-[#D3FF5C] flex items-center">
+        <p class="font-mono bg-black text-white p-1 px-4 rounded-xl">Images</p>
     </div>
-    <!-- TODO:: WRITE ABOUT  -->
-    <p class="bg-background p-4 font-mono font-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore iure reiciendis maxime facere officia architecto expedita illum ipsa tenetur? Debitis laborum sapiente id atque pariatur unde culpa quas voluptas eveniet? Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet qui asperiores, quod totam suscipit temporibus tenetur quas mollitia voluptatum maiores architecto, id adipisci aut. Blanditiis corporis culpa delectus repellendus molestias!</p>
+    <div class="m-2">
+
+        <Carousel
+        autoplay
+        autoplayDuration={2000}
+        let:loaded
+        >
+        {#each images as src, imageIndex (src)}
+            <div class="img-container">
+            {#if loaded.includes(imageIndex)}
+                <img {src} class="h-fit aspect-square object-cover" alt="nature" />
+            {/if}
+            </div>
+        {/each}
+        </Carousel>
+    </div>
+
 </div>
