6d942ffe149443005ecfde03c08420f02bb2d5a6
@acmahaja <acmahaja@gmail.com>
2024-02-16 12:24:03 +1100

    Fixed footer for desktop mode

--- src/sections/Footer.svelte
+++ src/sections/Footer.svelte
@@ -4,15 +4,18 @@
 
 </script>
 
-<footer class="w-full h-auto px-5 p-5 bg-black flex flex-col gap-2">
-    <p class="text-md text-primary font-mono font-bold">@acmahaja</p>
-    <p class="text-md text-white font-mono font-semibold">I'd love some feedback!</p>
-    <div class="flex flex-row gap-3 items-center">
-        {#each Links as Link}
-            <a href="{Link.URL}" id={Link.name} class="size-5">
-                <Icon class="size-5" icon="{Link.icon}" color="white"/>
-            </a>
-        {/each}
+
+<footer class="w-full h-auto text-md px-7 p-5 bg-black flex flex-col gap-2 lg:text-lg justify-center items-center lg:flex-row lg:justify-center">
+    <div class="w-full flex flex-col gap-2 lg:w-1/2 lg:max-w-[50rem]">
+        <p class="text-md text-primary font-mono font-bold">@acmahaja</p>
+        <p class="text-md text-white font-mono font-semibold">I'd love some feedback!</p>
+        <div class="flex flex-row gap-3 items-center">
+            {#each Links as Link}
+                <a href="{Link.URL}" id={Link.name} class="size-6">
+                    <Icon class="size-6" icon="{Link.icon}" color="white"/>
+                </a>
+            {/each}
+        </div>
     </div>
-    <p class="text-sm text-white opacity-50 font-mono font-semibold">Designed in Figma and developed using Neovim. Built with Svelte and Tailwind CSS, deployed to Google App Engine. Design inspired by <a href="https://brittanychiang.com/" class=" underline underline-offset-2">Brittany Chiang</a></p>
+    <p class="text-left text-white opacity-50 font-mono font-semibold lg:text-right lg:w-1/2 lg:max-w-[50rem]">Designed in Figma and developed using Neovim. Built with Svelte and Tailwind CSS, deployed to Google App Engine. Design inspired by <a href="https://brittanychiang.com/" class=" underline underline-offset-2">Brittany Chiang</a></p>
 </footer>
