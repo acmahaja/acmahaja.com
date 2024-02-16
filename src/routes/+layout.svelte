60e942203dd3df639445768acf337ec5d4add384
@acmahaja <acmahaja@gmail.com>
2024-02-16 13:20:14 +1100

    Migrated from svelte to sveltekit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	  import anime from "animejs";
+	  import Sidebar from "./components/Sidebar.svelte";
+
+	  import Header from "./sections/Header.svelte";
+	  import Footer from "./sections/Footer.svelte";
+	  import NavBar from "./components/NavBar.svelte";
+	  let pushDown:boolean = false;
+
+function togglePushDown() {
+  pushDown = !pushDown
+
+  console.log("toggle push down " + pushDown);
+
+  anime({
+	targets : '#main',
+	paddingTop: pushDown ? "12.75rem" : "4rem",
+	autoplay: true,
+  })
+}
+
+
+</script>
+<main id="main" class="app z-0 bg-background w-full min-h-screen h-full flex flex-col pt-16 lg:flex-row lg:items-start lg:justify-center">
+	<Header />
+
+	<NavBar />
+	<Sidebar on:change={togglePushDown} />
+	<main>
+		<slot />
+	</main>
+	<Footer />
+
+</main>
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import Header from '../sections/Header.svelte';
+	import About from '../sections/About.svelte';
+	import Education from '../sections/Education.svelte';
+	import Experience from '../sections/Experience.svelte';
+	import Technologies from '../sections/Technologies.svelte';
+	import Projects from '../sections/Projects.svelte';
+</script>
+
+<div
+	id="main"
+	class="z-0 bg-background w-full min-h-screen h-full flex flex-col pt-16 lg:flex-row lg:items-start lg:justify-center"
+>
+	<Header />
+	<div class="z-0 w-full h-fit flex flex-col gap-2 lg:w-8/12 lg:max-w-[66rem]">
+		<About />
+		<Education />
+		<Experience />
+		<Technologies />
+		<Projects />
+	</div>
+</div>
