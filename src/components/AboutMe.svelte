8ac74447e65dac4fe3b3b8aa4229bf8dc3906aa0
@acmahaja <acmahaja@gmail.com>
2024-02-19 22:00:21 +1100

    Modified About me

--- src/components/AboutMe.svelte
+++ src/components/AboutMe.svelte
@@ -11,10 +11,9 @@
 		<p class="font-mono bg-black text-white p-1 px-4 rounded-xl">About</p>
 	</div>
 	<p class="bg-background p-4 font-mono font-normal text-sm md:min-h-[200px]">
-		Hi! I'm Anjaney C Mahajan, a passionate Software Engineer! I love building user-friendly
+		Hi! I'm Anjaney C Mahajan, a design-driven Software Engineer! I love building user-friendly
 		full-stack applications using Java, JavaScript, and Python. Whether it's leading teams or
 		implementing agile practices, I enjoy every step. I've worked on projects ranging from aviation
 		to conservation. Feel free to reachout to work on something awesome together! 🚀
 	</p>
 </div>
-
--- src/sections/Education.svelte
+++ src/sections/Education.svelte
@@ -1,44 +1,33 @@
 <script lang="ts">
-  import Education from '../data/Education';
-  import moment from 'moment';
+	import Education from '../data/Education';
+	import moment from 'moment';
 </script>
 
-<section
-  id="background"
-  class="w-full min-h-fit h-fit p-8 py-4 flex flex-col gap-4"
->
-<hr
-class="w-full h-[1.5px] border-none bg-gradient-to-r from-background  to-[#76726e]"
-/>
+<section id="background" class="w-full min-h-fit h-fit p-8 py-4 flex flex-col gap-4">
+	<hr class="w-full h-[1.5px] border-none bg-gradient-to-r from-background to-[#76726e]" />
 
-  <h3 class="font-flex font-semibold text-2xl md:text-4xl md:font-semibold">
-    Education
-  </h3>
-  {#each Education as Ed}
-    <div
-      class="w-full bg-white max-w-xl p-2 flex flex-row justify-start items-center gap-3 border-black border-2 rounded-lg"
-    >
-      <img
-        src={Ed.logoImg}
-        alt={`${Ed.name} logo`}
-        style={`background-color: ${Ed.brandColour};`}
-        class="p-3 mt-1 size-[25%] rounded-md aspect-square max-w-24 max-h-24"
-      />
-      <div
-        class="flex flex-col items-start justify-start md:flex-row md:w-full md:justify-between md:items-center"
-      >
-        <div class="flex flex-col w-fit">
-          <p class="text-md font-medium md:text-lg">{Ed.name}</p>
-          <p class="text-sm italic md:text-md">{Ed.degree}</p>
-        </div>
-        <p
-          class="w-fit text-sm font-light italic opacity-80 md:text-right md:pr-2"
-        >
-          {moment(Ed.startDate).format('MMM YYYY')} - {moment(
-            Ed.endDate
-          ).format('MMM YYYY')}
-        </p>
-      </div>
-    </div>
-  {/each}
+	<h3 class="font-flex font-semibold text-2xl md:text-4xl md:font-semibold">Education</h3>
+	{#each Education as Ed}
+		<div
+			class="w-full bg-white max-w-xl p-2 flex flex-row justify-start items-center gap-3 border-black border-2 rounded-lg"
+		>
+			<img
+				src={Ed.logoImg}
+				alt={`${Ed.name} logo`}
+				style={`background-color: ${Ed.brandColour};`}
+				class="p-3 mt-1 size-[25%] rounded-md aspect-square max-w-24 max-h-24"
+			/>
+			<div
+				class="flex flex-col items-start justify-start md:flex-row md:w-full md:justify-between md:items-center"
+			>
+				<div class="flex flex-col w-fit">
+					<p class="text-md font-medium md:text-lg">{Ed.name}</p>
+					<p class="text-sm italic md:text-md">{Ed.degree}</p>
+				</div>
+				<p class="w-fit text-sm font-light italic opacity-80 md:text-right md:pr-2">
+					{moment(Ed.startDate).format('MMM YYYY')} - {moment(Ed.endDate).format('MMM YYYY')}
+				</p>
+			</div>
+		</div>
+	{/each}
 </section>
