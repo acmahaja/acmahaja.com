66d5f047ccd090605b2c0790a74169d06257d0d8
@acmahaja <acmahaja@gmail.com>
2024-02-19 16:09:13 +1100

    Added about me and personal photos links

--- src/components/AboutMe.svelte
+++ src/components/AboutMe.svelte
@@ -1,9 +1,20 @@
 <script lang="ts"></script>
 
-<div id="AboutDescription" class="w-full min-h-fit bg-white font-flex font-medium border-solid border-black border-[1px] rounded-md flex flex-col 2xl:w-[43%]">
-    <div id="AboutTitlebar" class=" h-fit border-solid size-8 p-2 w-full border-black border-b-[1px] px-3 bg-[#D3FF5C] flex items-center">
-        <p class="font-mono bg-black text-white p-1 px-4 rounded-xl">About</p>
-    </div>
-    <!-- TODO:: WRITE ABOUT  -->
-    <p class="bg-background p-4 font-mono font-normal text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore iure reiciendis maxime facere officia architecto expedita illum ipsa tenetur? Debitis laborum sapiente id atque pariatur unde culpa quas voluptas eveniet? Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet qui asperiores, quod totam suscipit temporibus tenetur quas mollitia voluptatum maiores architecto, id adipisci aut.</p>
+<div
+	id="AboutDescription"
+	class="w-full min-h-fit bg-white font-flex font-medium border-solid border-black border-[1px] rounded-md flex flex-col 2xl:w-[43%]"
+>
+	<div
+		id="AboutTitlebar"
+		class=" h-fit border-solid size-8 p-2 w-full border-black border-b-[1px] px-3 bg-[#D3FF5C] flex items-center"
+	>
+		<p class="font-mono bg-black text-white p-1 px-4 rounded-xl">About</p>
+	</div>
+	<p class="bg-background p-4 font-mono font-normal text-sm md:min-h-[200px]">
+		Hi! I'm Anjaney C Mahajan, a passionate Software Engineer! I love building user-friendly
+		full-stack applications using Java, JavaScript, and Python. Whether it's leading teams or
+		implementing agile practices, I enjoy every step. I've worked on projects ranging from aviation
+		to conservation. Feel free to reachout to work on something awesome together! 🚀
+	</p>
 </div>
+
--- src/data/Images.ts
+++ src/data/Images.ts
@@ -1,6 +1,7 @@
-const images:string[] = [
-  'https://res.cloudinary.com/www-acmahaja-com/image/upload/v1657613135/cld-sample.jpg',
-  'https://res.cloudinary.com/www-acmahaja-com/image/upload/v1657613136/cld-sample-2.jpg',
+const images: string[] = [
+  'https://res.cloudinary.com/www-acmahaja-com/image/upload/v1708318373/acmahaja.com/bblwoesqdthnjvzitrlp.webp',
+  'https://res.cloudinary.com/www-acmahaja-com/image/upload/ar_1:1,c_crop,g_face,w_2500/acmahaja.com/bqgnibucaroxndrwnxjc.jpg',
+  'https://res.cloudinary.com/www-acmahaja-com/image/upload/ar_1:1,c_crop,g_face,w_2500/acmahaja.com/kvmwauagqxdby0we4kb7.jpg'
 ]
 
 export default images;
