8321f4ee498646ccdc8e2242589259e1c6907e96
acmahaja <acmahaja@pop-os.local>
2024-03-25 09:24:26 +1100

    Hidden some tech

--- src/data/Technologies.ts
+++ src/data/Technologies.ts
@@ -1,222 +1,221 @@
-import type { TechnologyType } from "../types/Technologies";
+import type { TechnologyType } from '../types/Technologies';
 
 export const reactTech: TechnologyType = {
-  name: 'react',
-  iconifyLogo: 'react',
-  color: '#61dafb',
-  hoverColor: '#000'
-}
-
-export const svelteTech: TechnologyType = {
-  name: 'svelte',
-  iconifyLogo: 'svelte',
-  color: '#ff4000',
-  hoverColor: '#fff'
-}
-
-export const javaTech: TechnologyType = {
-  name: 'Java',
-  iconifyLogo: 'java',
-  color: '#007396',
-  hoverColor: '#000'
-}
-
-export const javascriptTech: TechnologyType = {
-  name: 'JavaScript',
-  iconifyLogo: 'javascript',
-  color: '#f7df1e',
-  hoverColor: '#000'
-}
-
-export const pythonTech: TechnologyType = {
-  name: 'Python',
-  iconifyLogo: 'python',
-  color: '#3776ab',
-  hoverColor: '#000'
-}
-
-export const nodejsTech: TechnologyType = {
-  name: 'Node.js',
-  iconifyLogo: 'nodejs',
-  color: '#339933',
-  hoverColor: '#000'
-}
-
-export const djangoTech: TechnologyType = {
-  name: 'Django',
-  iconifyLogo: 'djangorest',
-  color: '#092e20',
-  hoverColor: '#fff'
-}
-
-export const mongodbTech: TechnologyType = {
-  name: 'MongoDB',
-  iconifyLogo: 'mongodb',
-  color: '#47a248',
-  hoverColor: '#fff'
-}
-
-export const javaVertxTech: TechnologyType = {
-  name: 'Java Vert.x',
-  iconifyLogo: 'vertx',
-  color: '#007396',
-  hoverColor: '#fff'
-}
-
-export const figmaTech: TechnologyType = {
-  name: 'Figma',
-  iconifyLogo: 'figma',
-  color: '#0AC6FF',
-  hoverColor: '#fff'
-}
-
-export const htmlTech: TechnologyType = {
-  name: 'HTML',
-  iconifyLogo: 'html5',
-  color: '#e34c26',
-  hoverColor: '#fff'
-}
-
-export const cssTech: TechnologyType = {
-  name: 'CSS',
-  iconifyLogo: 'css3',
-  color: '#1572B6',
-  hoverColor: '#fff'
-}
-
-export const tensorflowTech: TechnologyType = {
-  name: 'TensorFlow',
-  iconifyLogo: 'tensorflow',
-  color: '#FF6F00',
-  hoverColor: '#fff'
-}
-
-export const firebaseTech: TechnologyType = {
-  name: 'Firebase',
-  iconifyLogo: 'firebase',
-  color: '#FFCA28',
-  hoverColor: '#000'
-}
-
-export const raspberryPiTech: TechnologyType = {
-  name: 'Raspberry Pi',
-  iconifyLogo: 'raspberrypi',
-  color: '#C51A4A',
-  hoverColor: '#fff'
-}
-
-export const googleCloudTech: TechnologyType = {
-  name: 'Google Cloud Platform',
-  iconifyLogo: 'googlecloud',
-  color: '#4285F4',
-  hoverColor: '#fff'
-}
-
-export const postgresqlTech: TechnologyType = {
-  name: 'PostgreSQL',
-  iconifyLogo: 'postgresql',
-  color: '#336791',
-  hoverColor: '#fff'
-}
-
-export const mysqlTech: TechnologyType = {
-  name: 'MySQL',
-  iconifyLogo: 'mysql',
-  color: '#4479A1',
-  hoverColor: '#fff'
-}
-
-export const nextjsTech: TechnologyType = {
-  name: 'Next.js',
-  iconifyLogo: 'nextjs',
-  color: '#000000',
-  hoverColor: '#fff'
-}
-
-export const typescriptTech: TechnologyType = {
-  name: 'Typescript',
-  iconifyLogo: 'typescript',
-  color: '#007ACC',
-  hoverColor: '#fff'
-}
-
-export const tailwindCssTech: TechnologyType = {
-  name: 'Tailwind CSS',
-  iconifyLogo: 'tailwindcss',
-  color: '#38B2AC',
-  hoverColor: '#fff'
-}
-
-export const bootstrapTech: TechnologyType = {
-  name: 'Bootstrap',
-  iconifyLogo: 'bootstrap',
-  color: '#7952B3',
-  hoverColor: '#fff'
-}
-
-export const dockerTech: TechnologyType = {
-  name: 'Docker',
-  iconifyLogo: 'docker',
-  color: '#2496ED',
-  hoverColor: '#fff'
-}
-
-export const gitTech: TechnologyType = {
-  name: 'Git',
-  iconifyLogo: 'git',
-  color: '#F05032',
-  hoverColor: '#fff'
-}
-
-export const githubTech: TechnologyType = {
-  name: 'GitHub',
-  iconifyLogo: 'github',
-  color: '#c9d1d9',
-  hoverColor: '#000'
-}
-
-export const viteTech = {
-  name: 'Vite',
-  iconifyLogo: 'vitejs',
-  color: '#646CFF',
-  hoverColor: '#fff'
+	name: 'react',
+	iconifyLogo: 'react',
+	color: '#61dafb',
+	hoverColor: '#000'
 };
 
+export const svelteTech: TechnologyType = {
+	name: 'svelte',
+	iconifyLogo: 'svelte',
+	color: '#ff4000',
+	hoverColor: '#fff'
+};
+
+export const javaTech: TechnologyType = {
+	name: 'Java',
+	iconifyLogo: 'java',
+	color: '#007396',
+	hoverColor: '#000'
+};
+
+export const javascriptTech: TechnologyType = {
+	name: 'JavaScript',
+	iconifyLogo: 'javascript',
+	color: '#f7df1e',
+	hoverColor: '#000'
+};
+
+export const pythonTech: TechnologyType = {
+	name: 'Python',
+	iconifyLogo: 'python',
+	color: '#3776ab',
+	hoverColor: '#000'
+};
+
+export const nodejsTech: TechnologyType = {
+	name: 'Node.js',
+	iconifyLogo: 'nodejs',
+	color: '#339933',
+	hoverColor: '#000'
+};
+
+export const djangoTech: TechnologyType = {
+	name: 'Django',
+	iconifyLogo: 'djangorest',
+	color: '#092e20',
+	hoverColor: '#fff'
+};
+
+export const mongodbTech: TechnologyType = {
+	name: 'MongoDB',
+	iconifyLogo: 'mongodb',
+	color: '#47a248',
+	hoverColor: '#fff'
+};
+
+export const javaVertxTech: TechnologyType = {
+	name: 'Java Vert.x',
+	iconifyLogo: 'vertx',
+	color: '#007396',
+	hoverColor: '#fff'
+};
+
+export const figmaTech: TechnologyType = {
+	name: 'Figma',
+	iconifyLogo: 'figma',
+	color: '#0AC6FF',
+	hoverColor: '#fff'
+};
+
+export const htmlTech: TechnologyType = {
+	name: 'HTML',
+	iconifyLogo: 'html5',
+	color: '#e34c26',
+	hoverColor: '#fff'
+};
+
+export const cssTech: TechnologyType = {
+	name: 'CSS',
+	iconifyLogo: 'css3',
+	color: '#1572B6',
+	hoverColor: '#fff'
+};
+
+export const tensorflowTech: TechnologyType = {
+	name: 'TensorFlow',
+	iconifyLogo: 'tensorflow',
+	color: '#FF6F00',
+	hoverColor: '#fff'
+};
+
+export const firebaseTech: TechnologyType = {
+	name: 'Firebase',
+	iconifyLogo: 'firebase',
+	color: '#FFCA28',
+	hoverColor: '#000'
+};
+
+export const raspberryPiTech: TechnologyType = {
+	name: 'Raspberry Pi',
+	iconifyLogo: 'raspberrypi',
+	color: '#C51A4A',
+	hoverColor: '#fff'
+};
+
+export const googleCloudTech: TechnologyType = {
+	name: 'Google Cloud Platform',
+	iconifyLogo: 'googlecloud',
+	color: '#4285F4',
+	hoverColor: '#fff'
+};
+
+export const postgresqlTech: TechnologyType = {
+	name: 'PostgreSQL',
+	iconifyLogo: 'postgresql',
+	color: '#336791',
+	hoverColor: '#fff'
+};
+
+export const mysqlTech: TechnologyType = {
+	name: 'MySQL',
+	iconifyLogo: 'mysql',
+	color: '#4479A1',
+	hoverColor: '#fff'
+};
+
+export const nextjsTech: TechnologyType = {
+	name: 'Next.js',
+	iconifyLogo: 'nextjs',
+	color: '#000000',
+	hoverColor: '#fff'
+};
+
+export const typescriptTech: TechnologyType = {
+	name: 'Typescript',
+	iconifyLogo: 'typescript',
+	color: '#007ACC',
+	hoverColor: '#fff'
+};
+
+export const tailwindCssTech: TechnologyType = {
+	name: 'Tailwind CSS',
+	iconifyLogo: 'tailwindcss',
+	color: '#38B2AC',
+	hoverColor: '#fff'
+};
+
+export const bootstrapTech: TechnologyType = {
+	name: 'Bootstrap',
+	iconifyLogo: 'bootstrap',
+	color: '#7952B3',
+	hoverColor: '#fff'
+};
+
+export const dockerTech: TechnologyType = {
+	name: 'Docker',
+	iconifyLogo: 'docker',
+	color: '#2496ED',
+	hoverColor: '#fff'
+};
+
+export const gitTech: TechnologyType = {
+	name: 'Git',
+	iconifyLogo: 'git',
+	color: '#F05032',
+	hoverColor: '#fff'
+};
+
+export const githubTech: TechnologyType = {
+	name: 'GitHub',
+	iconifyLogo: 'github',
+	color: '#c9d1d9',
+	hoverColor: '#000'
+};
+
+export const viteTech = {
+	name: 'Vite',
+	iconifyLogo: 'vitejs',
+	color: '#646CFF',
+	hoverColor: '#fff'
+};
 
 export const expressjsTech = {
-  name: 'Express.js',
-  iconifyLogo: 'express',
-  color: '#04af2f',
-  hoverColor: '#fff'
+	name: 'Express.js',
+	iconifyLogo: 'express',
+	color: '#04af2f',
+	hoverColor: '#fff'
 };
 
 const Technologies = [
-  reactTech,
-  svelteTech,
-  javaTech,
-  javaVertxTech,
-  javascriptTech,
-  typescriptTech,
-  pythonTech,
-  nodejsTech,
-  expressjsTech,
-  djangoTech,
-  mongodbTech,
-  figmaTech,
-  htmlTech,
-  cssTech,
-  tensorflowTech,
-  firebaseTech,
-  raspberryPiTech,
-  googleCloudTech,
-  postgresqlTech,
-  mysqlTech,
-  nextjsTech,
-  tailwindCssTech,
-  bootstrapTech,
-  dockerTech,
-  gitTech,
-  githubTech
+	reactTech,
+	svelteTech,
+	javaTech,
+	// javaVertxTech,
+	javascriptTech,
+	typescriptTech,
+	pythonTech,
+	nodejsTech,
+	expressjsTech,
+	// djangoTech,
+	mongodbTech,
+	figmaTech,
+	htmlTech,
+	cssTech,
+	// tensorflowTech,
+	// firebaseTech,
+	raspberryPiTech,
+	googleCloudTech,
+	postgresqlTech,
+	// mysqlTech,
+	// nextjsTech,
+	tailwindCssTech,
+	bootstrapTech,
+	dockerTech,
+	gitTech,
+	githubTech
 ];
 
 export default Technologies;
